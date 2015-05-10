51c32ba7e1bc097abb3a54d0dfff815b6fd0e73a
Adam Herbst <adh38@cornell.edu>
2015-05-10 09:36:56 -0700

    prior to pulling code

---
+++ src/T4TApp.cpp
@@ -0,0 +1,80 @@
+#include "T4TApp.h"
+
+// Declare our game instance
+T4TApp game;
+
+T4TApp::T4TApp()
+    : _scene(NULL), _wireframe(false)
+{
+}
+
+void T4TApp::initialize()
+{
+    // Load game scene from file
+    _scene = Scene::load("res/demo.scene");
+
+    // Get the box model and initialize its material parameter values and bindings
+    Node* boxNode = _scene->findNode("box");
+    Model* boxModel = dynamic_cast<Model*>(boxNode->getDrawable());
+    Material* boxMaterial = boxModel->getMaterial();
+
+    // Set the aspect ratio for the scene's camera to match the current resolution
+    _scene->getActiveCamera()->setAspectRatio(getAspectRatio());
+}
+
+void T4TApp::finalize()
+{
+    SAFE_RELEASE(_scene);
+}
+
+void T4TApp::update(float elapsedTime)
+{
+    // Rotate model
+    _scene->findNode("box")->rotateY(MATH_DEG_TO_RAD((float)elapsedTime / 1000.0f * 180.0f));
+}
+
+void T4TApp::render(float elapsedTime)
+{
+    // Clear the color and depth buffers
+    clear(CLEAR_COLOR_DEPTH, Vector4::zero(), 1.0f, 0);
+
+    // Visit all the nodes in the scene for drawing
+    _scene->visit(this, &T4TApp::drawScene);
+}
+
+bool T4TApp::drawScene(Node* node)
+{
+    // If the node visited contains a drawable object, draw it
+    Drawable* drawable = node->getDrawable(); 
+    if (drawable)
+        drawable->draw(_wireframe);
+
+    return true;
+}
+
+void T4TApp::keyEvent(Keyboard::KeyEvent evt, int key)
+{
+    if (evt == Keyboard::KEY_PRESS)
+    {
+        switch (key)
+        {
+        case Keyboard::KEY_ESCAPE:
+            exit();
+            break;
+        }
+    }
+}
+
+void T4TApp::touchEvent(Touch::TouchEvent evt, int x, int y, unsigned int contactIndex)
+{
+    switch (evt)
+    {
+    case Touch::TOUCH_PRESS:
+        _wireframe = !_wireframe;
+        break;
+    case Touch::TOUCH_RELEASE:
+        break;
+    case Touch::TOUCH_MOVE:
+        break;
+    };
+}
